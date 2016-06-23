ccaa83fae40981060e36fc5d0d736b87b8fb5267
Gavin Wiener <gavinwiener@gmail.com>
2016-06-23 23:08:19 +0200

    Removed Google tracking and footer detail

--- templates/footer.php
+++ templates/footer.php
@@ -1,16 +1,15 @@
 <footer class="footer_2">
-	
+
 	<!-- upper footer -->
 	<div class="footer-colour">
-		
-	
+
+
 	<div class="col-md-12 col-xs-12 text-left">
 
-		&copy; <?php echo date("Y"); ?> L. Wiener &amp; Associates. All Rights Reserved   
+		&copy; <?php echo date("Y"); ?> Vivamus rhoncus gravida ipsum ac condimentum.   
 
 	</div>
 
-	</div> 
+	</div>
 
 </footer>
-
